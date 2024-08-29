90041a809ad92e432546b8c5d4b3099d03970cd0
DigiDagin <149829632+digidaginGS@users.noreply.github.com>
2024-08-29 18:10:56 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,14 @@ import Script from "next/script";
 const inter = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons: {
+    shortcut: [
+      {
+        url: "favicon.ico", // Используйте 'url' вместо 'src'
+        type: "image/x-icon",
+      },
+    ],
+  },
   title: "Verde: Разработка Нефтегазовых Месторождений",
   description:
     "Verde - оператор управления активами в нефтегазовой отрасли. Специализируемся на разработке месторождений, управлении активами, инновационных технологиях и обеспечении экологической и социальной ответственности.",
@@ -32,6 +40,22 @@ export const metadata: Metadata = {
     "проект",
     "баррелей",
   ],
+  openGraph: {
+    title: "Verde: Эффективная Разработка Нефтегазовых Месторождений",
+    description:
+      "Verde - ваш надежный партнер в нефтегазовой отрасли. Мы предлагаем комплексные решения для разработки месторождений, управления активами и внедрения инновационных технологий с фокусом на экологическую и социальную ответственность.", // Более подробное описание
+    url: "https://www.verdeg.com",
+    type: "website",
+    images: [
+      {
+        url: "https://verdeg.com/assets/hero/background2.webp",
+        width: 1200,
+        height: 630,
+        alt: "Verde: Разработка Нефтегазовых Месторождений",
+      },
+    ],
+    siteName: "Verde",
+  },
 };
 
 export default function RootLayout({
@@ -43,9 +67,7 @@ export default function RootLayout({
     <html lang="ru">
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ClientWrapper>
-            {children}
-          </ClientWrapper>
+          <ClientWrapper>{children}</ClientWrapper>
         </ConvexClientProvider>
       </body>
     </html>
